6bcb2f6a7ae33d2562945a00162f1538fe3fbbde
SmSm <86024087+DEVSMSM@users.noreply.github.com>
2022-11-29 18:39:22 +0000

    New Updates

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 let words = [
-  `History Of ME`,
-  `HI Guys`,
-  `Im Good And YOU`,
-  `Im Fine LOOL`,
-  `So How Was Your Day`,
-  `It Was Very Good`,
-  `IM SO Mad Idk Why`,
-  `I Should Go School`,
-  `Do You Have Any Think To Say`,
-  `No Thanks I Must GO BBye`
- 
+
+  `When you reach the end of your rope, tie a knot in it and hang on`,
+  `Don't judge each day by the harvest you reap but by the seeds that you plant.`,
+  `The future belongs to those who believe in the beauty of their dreams.`,
+  `It is during our darkest moments that we must focus to see the light.`,
+  `Spread love everywhere you go. Let no one ever come to you without leaving happier.`,
+  `When you reach the end of your rope, tie a knot in it and hang on.`,
+  `Always remember that you are absolutely unique. Just like everyone else.`,
+  `Don't judge each day by the harvest you reap but by the seeds that you plant.`,
+  `The future belongs to those who believe in the beauty of their dreams.`,
+  `Tell me and I forget. Teach me and I remember. Involve me and I learn.`,
+  `The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.`,
+  `It is during our darkest moments that we must focus to see the light.`,
+  `Whoever is happy will make others happy too`,
+  `Do not go where the path may lead, go instead where there is no path and leave a trail.`,
+  `Famous Quotes About Life
+  You will face many defeats in life, but never let yourself be defeated.`,
+  `Do not go where the path may lead, go instead where there is no path and leave a trail.`,
+  `Never let the fear of striking out keep you from playing the game.`,
+  `Life is never fair, and perhaps it is a good thing for most of us that it is not.`,
+  `The only impossible journey is the one you never begin.`,
+  `In this life we cannot do great things. We can only do small things with great love.`,
+  `The greatest glory in living lies not in never falling, but in rising every time we fall.`,
+  `Life is made of ever so many partings welded together.`,
+  `The secret of success is to do the common thing uncommonly well.`,
+  `I never dreamed about success, I worked for it.`,
+  `An unexamined life is not worth living.`
 ]
 
 let ChOwO = [
@@ -61,21 +77,24 @@ setInterval(async() => {
 } catch (error) {
   console.log(error)
 }
-}, ms);
+}, ms)
+
+
+
 
 
 setInterval(async() => {
-  try{
-if(!Client || Client.user ) {
+try{
+if(!Client || !Client.user ) {
   console.log("The Client Didn't Login Killing Procces")
-  process.kill(1);
+  await process.kill(1)
 } else {
 
 }
   } catch {
 
   }
-}, 60000)
+}, 90000)
 
 
  }
